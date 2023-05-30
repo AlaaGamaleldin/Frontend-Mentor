65b6b5b93707277c8d58385b0d0dc0f2d8a7cd67
Alaa Gamal <alaadesigner85@gmail.com>
2023-05-30 17:50:27 +0300

    not done

---
+++ main.js
@@ -0,0 +1,28 @@
+let subscrib = document.getElementById("#subscrbe-btn");
+
+let input1 = document.getElementById("#sub-btn");
+let requerd = document.getElementById("#requerd");
+function subscribeBtn(e){
+   
+    if(input1.value === " "){
+        requerd.style.display = "block";
+        input1.style.border =  "1px solid red";
+        return false;
+        
+    }
+    e.preventDefault();
+}
+
+/*
+    let main = document.getElementsByClassName(".main-container");
+    let success = document.getElementsByClassName(".success-active");
+    
+    let dismiss = document.getElementById("#dismiss");
+
+    
+    
+    
+   
+    
+    
+        */
